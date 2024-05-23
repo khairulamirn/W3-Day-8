35738cb3f0439f186528b52b5bf984b9f323cf12
TheKiruls <amirin856@gmail.com>
2024-05-23 16:01:44 +0800

    bmi-calculator

---
+++ bmi-calculator/script.js
@@ -0,0 +1,71 @@
+//  this is an example of pseudocode
+//  pseudocode is a way of planning out your code before you write it
+
+// BMI calculator web app
+// BMI is calculated by dividing a person's weight in kilograms by the square of his/her height in meters
+// BMI = kg/m^2
+// BMI = kg/m/m
+// BMI = kg/(m*m)
+
+// category	BMI (kg/m2)
+// 1. Very severely underweight	< 15
+// 2. Severely underweight	< 16
+// 3. Underweight	< 18.5
+// 4. Normal (healthy weight)	18.5 - 25
+// 5. Overweight	25 - 30
+// 6. Obese Class I (Moderately obese)	30 - 35
+// 7. Obese Class II (Severely obese)	35 - 40
+// 8. Obese Class III (Very severely obese)	> 40
+
+// how our app will work:
+// input user for weight in kilograms
+// input user for height in meters
+// calculate BMI based on user input (weight and height) when user click the button
+// display BMI at h2 with id="bmiValue" by execute the calculation when user click the button
+// display BMI category at h2 with id="bmiCategory" by using if else statement
+
+const button = document.querySelector("#button");
+const weightInput = document.querySelector("#weight");
+const heightInput = document.querySelector("#height");
+const bmiCategory = document.querySelector("#bmiCategory");
+const bmiValue = document.querySelector("#bmiValue");
+
+
+weight.addEventListener("input" , function (e) {
+    console.log(e.target.value);
+});
+
+height.addEventListener("input" , function (e) {
+    console.log(e.target.value);
+});
+
+button.addEventListener("click", function () {
+    console.log("Button clicked");
+
+const weight = weightInput.value;
+const height = heightInput.value;
+
+let bmi = (weight / (height*height));
+    if (bmi < 15) {
+        category = "Very severely underweight"; 
+    } else if (bmi >= 15 && bmi < 16) {
+        category = "Severely underweight";; 
+    } else if (bmi >= 16 && bmi < 18.5) {
+        category = "Underweight";
+    } else if (bmi >=18.5 && bmi < 25) {
+        category ="Normal (healthy weight)"
+    } else if (bmi >=25 && bmi < 30) {
+        category ="Overweight"
+    } else if (bmi >=30 && bmi < 35) {
+        category ="Obese Class I (Moderately obese)"
+    } else if (bmi >= 35 && bmi < 40) {
+        category = "Obese Class II (Severely obese)";
+    } else {
+        category = "Obese Class III (Very severely obese)";
+    }
+
+bmiCategory.innerText = category; 
+bmiValue.innerText = bmi;
+
+}) 
+
--- dom-intro/script.js
+++ dom-intro/script.js
@@ -26,20 +26,21 @@ h1ByClassTitle.style.backgroundColor = "red";
 h1ByClassTitle.style.color = "white";
 h1ByClassTitle.style.padding = "20px";
 
+// IMPORTANT: Assignment of elements to variables must be done initially
 const text = document.querySelector("#text");
 const nameInput = document.querySelector("#nameInput");
 const submitName = document.querySelector("#submitName");
 
 // 1. get the value of the input using event listener
 // event in JavaScript 
-// onsubmit, onclick, onmouseover, 
+// onsubmit, onclick, onmouseover, onmouseout, onkeydown, onkeyup, onkeypress 
 // when use fill the input -> oninput, onchange
 // accept 2 arguments: event name, callback function 
 nameInput.addEventListener("input", function (e) {
     console.log(e.target.value);
 });
 
-// add event listener to the button
+// 2. add event listener to the button
 submitName.addEventListener("click", function () {
     console.log("Button clicked");
     const name = nameInput.value;
@@ -64,10 +65,19 @@ function greetWithName (name) {
 
 // 4. function invocation with argument
 greetWithName(" John");
+greetWithName("Doe");
+greetWithName("Jane");
 
 // 5. function declaration with multiple parameters 
+function greetWithNameAndAge(name, age) {
+    console.log("Hello " + name + ", you are " + age + " years old");
+  }
 
 // 6. function invocation with multiple arguments 
+greetWithNameAndAge("John", 25);
+const test = greetWithNameAndAge("Doe", 30);
+// test is undefined because the function does not return anything
+console.log(test);
 
 // 7. function declaration with return value 
 function add(a, b) {
